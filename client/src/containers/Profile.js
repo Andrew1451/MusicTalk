6e12b22dd20750289b580815e8fb4d432ca4868a
Andrew <andrew.gaynor@ymail.com>
2020-12-26 17:14:42 -0500

    style profile page for mobile

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import './Profile.css';
+import classes from './Profile.module.css';
 import * as actions from '../store/actions/index';
 
 const Profile = ({state, onLogin}) => {
@@ -10,9 +10,40 @@ const Profile = ({state, onLogin}) => {
         }
     })
 
+    const placeholderFriends = ['MusicMan', 'LooneyTunes', 'MajorMinor', 'guitarguy', 'OfficialMozart', 'iLoveMusic']
+
     return (
-        <div>
-            <h3>Hi, {state.user}!</h3>
+        <div className={classes.Profile}>
+            <h2>Hi, {state.user}!</h2>
+            <section>
+                <h3>Friends</h3>
+                {/* <hr></hr> */}
+                <div className={classes.FriendsList}>
+                    {placeholderFriends.map((friend, i) => {
+                        return <p className={classes.Friend} key={i}>{friend}</p>
+                    })}
+                </div>
+                <div className={classes.Buttons}>
+                    <button type='button'>View Friends</button>
+                    <button type='button'>Find Friends</button>
+                </div>
+            </section>
+            <section>
+                <h3>Posts</h3>
+                <div className={classes.PostContainer}>
+                    <div className={classes.PostName}>
+                        <p>{state.user}</p>
+                    </div>
+                    <div className={classes.Post}>
+                        <p>Music - vocal or instrumental sounds combined in such a way as to produce beauty of form, 
+                            harmony, and expression of emotion.</p>
+                    </div>
+                    <div className={classes.LikeComment}>
+                        <div>Like</div>
+                        <div style={{borderRight: 'none'}}>Comment</div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
