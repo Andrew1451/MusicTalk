712f65be66fb08cc1e40fc734b747a2c4b62f2db
Andrew <andrew.gaynor@ymail.com>
2021-01-03 13:17:23 -0500

    style friends page

--- client/src/containers/Friends.js
+++ client/src/containers/Friends.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import classes from './Friends.module.css';
 
 const Friends = props => {
+    const placeholderFriends = ['MusicMan', 'LooneyTunes', 'MajorMinor', 'guitarguy', 'OfficialMozart', 'iLoveMusic']
     return (
-        <div>
-            
+        <div className={classes.FriendsPage}>
+            <h1>Your Friends</h1>
+            <button type='button' className={classes.FindFriends}>Find Friends</button>
+            <hr></hr>
+            <input type='text' placeholder='Search Friends'></input>
+            <button type='button' className={classes.SearchButton}>Search</button>
+            <ul>
+            {placeholderFriends.map((username, i) => {
+               return <li key={i}>{username}</li> 
+            })}
+            </ul>
         </div>
     )
 }
